3b9321d04fda5bcc8226b06935f1e9ecc697b998
Johnnie Munger <johnnie@wonderful.io>
2021-01-04 11:30:04 -0800

    removes unnecessary methods

--- src/analyzers/seo.js
+++ src/analyzers/seo.js
@@ -1,13 +1,5 @@
 const lighthouse = require('../lib/lighthouse');
 
-function scorePasses(score) {
-	return (score >= 90);
-}
-
-function normalizeScore(score) {
-	return score * 100;
-}
-
 export async function run(url) {
   const runnerResult = await lighthouse.runLighthouse(url,['seo']);
   lighthouse.logResultsOfAnalysis(runnerResult, 'SEO Audit', 'seo');
